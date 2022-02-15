0ecfd2d78400261c13b293564cee113344c73b82
Antonio Lo Fiego <alofiego@hey.com>
2022-02-15 01:27:12 -0500

    Add dark-theme support

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,19 +3,22 @@ import { Toaster } from 'react-hot-toast';
 import { SWRConfig } from 'swr';
 import axios from 'axios';
 import CartProvider from '../context/cart';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url) => axios.get(url).then((res) => res.data),
-      }}
-    >
-      <CartProvider>
-        <Toaster position='bottom-left' />
-        <Component {...pageProps} />
-      </CartProvider>
-    </SWRConfig>
+    <ThemeProvider attribute='class'>
+      <SWRConfig
+        value={{
+          fetcher: (url) => axios.get(url).then((res) => res.data),
+        }}
+      >
+        <CartProvider>
+          <Toaster position='bottom-left' />
+          <Component {...pageProps} />
+        </CartProvider>
+      </SWRConfig>
+    </ThemeProvider>
   );
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import { Cart, AddToCart } from '../components';
 
@@ -8,6 +9,10 @@ import { useWithToast } from '../hooks';
 
 const Home = () => {
   const [pageIndex, setPageIndex] = useState(1);
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
 
   const { data, isLoading } = useWithToast(
     useSWR(`https://swapi.dev/api/starships?page=${pageIndex}`)
@@ -16,84 +21,113 @@ const Home = () => {
   const MAX_PAGES = Math.round(data?.count / 10, 0) - 1;
   const MIN_PAGES = 1;
 
+  if (!mounted) return null;
+
+  const ThemeSwitcher = () => {
+    return (
+      <>
+        {resolvedTheme === 'dark' ? (
+          <button
+            className='fixed top-0 right-0 p-4 text-5xl'
+            onClick={() => setTheme('light')}
+          >
+            ☀️
+          </button>
+        ) : (
+          <button
+            className='fixed top-0 right-0 p-4 text-5xl'
+            onClick={() => setTheme('dark')}
+          >
+            🌚
+          </button>
+        )}
+      </>
+    );
+  };
+
   return (
-    <main className='flex items-center justify-center'>
-      <div className='space-y-8'>
-        <h1 className='text-4xl text-center'>Star Wars Starship Store</h1>
-        {isLoading && <p>Loading</p>}
-        <div className='grid grid-cols-2 gap-4'>
-          {data &&
-            data.results.map((starship) => {
-              const id =
-                starship.url.split('/')[starship.url.split('/').length - 2];
-              const price = parseInt(starship.cost_in_credits);
-              return (
-                <div
-                  key={starship.name}
-                  className='flex items-center justify-between'
-                >
-                  <div className='p-2 mx-2 space-y-2'>
-                    <h2 className='text-xl'>{starship.name}</h2>
-                    <h3 className='text-lg'>{starship.model}</h3>
+    <>
+      <ThemeSwitcher />
+      <main className='flex items-center justify-center'>
+        <div className='space-y-8'>
+          <h1 className='py-4 text-5xl font-extrabold text-center text-transparent bg-clip-text bg-gradient-to-br dark:from-white from-black to-yellow-600'>
+            Star Wars Starship Store
+          </h1>
+          {isLoading && <p>Loading</p>}
+          <div className='grid grid-cols-2 gap-4'>
+            {data &&
+              data.results.map((starship) => {
+                const id =
+                  starship.url.split('/')[starship.url.split('/').length - 2];
+                const price = parseInt(starship.cost_in_credits);
+                return (
+                  <div
+                    key={starship.name}
+                    className='flex items-center justify-between'
+                  >
+                    <div className='p-2 mx-2 space-y-2'>
+                      <h2 className='text-xl'>{starship.name}</h2>
+                      <h3 className='text-lg'>{starship.model}</h3>
+                    </div>
+                    <div className='flex flex-col items-end justify-end'>
+                      <p>
+                        {isNaN(price) ? (
+                          'Unavailable'
+                        ) : (
+                          <span className='flex items-center'>
+                            <Image
+                              src='/images/credit.png'
+                              alt='Star Wars Credit'
+                              height='16px'
+                              width='12px'
+                            />
+                            {price.toLocaleString()}
+                          </span>
+                        )}
+                      </p>
+                      <AddToCart id={id} price={price} name={starship.name} />
+                    </div>
                   </div>
-                  <div className='flex flex-col items-end justify-end'>
-                    <p>
-                      {isNaN(price) ? (
-                        'Unavailable'
-                      ) : (
-                        <span className='flex items-center'>
-                          <Image
-                            src='/images/credit.png'
-                            alt='Star Wars Credit'
-                            height='16px'
-                            width='12px'
-                          />
-                          {price.toLocaleString()}
-                        </span>
-                      )}
-                    </p>
-                    <AddToCart id={id} price={price} name={starship.name} />
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-        <div className='flex items-center justify-center space-x-3'>
-          <button
-            disabled={pageIndex === MIN_PAGES}
-            className='disabled:text-gray-400'
-            onClick={() => setPageIndex(MIN_PAGES)}
-          >
-            Go to first
-          </button>
+                );
+              })}
+          </div>
+          <div className='flex items-center justify-center space-x-3'>
+            <button
+              disabled={pageIndex === MIN_PAGES}
+              className='disabled:text-gray-400'
+              onClick={() => setPageIndex(MIN_PAGES)}
+            >
+              Go to first
+            </button>
 
-          <button
-            disabled={pageIndex === MIN_PAGES}
-            className='disabled:text-gray-400'
-            a
-            onClick={() => setPageIndex((pageIndex) => pageIndex - 1)}
-          >
-            Previous
-          </button>
+            <button
+              disabled={pageIndex === MIN_PAGES}
+              className='disabled:text-gray-400'
+              a
+              onClick={() => setPageIndex((pageIndex) => pageIndex - 1)}
+            >
+              Previous
+            </button>
 
-          <button
-            disabled={pageIndex === MAX_PAGES}
-            className='disabled:text-gray-400'
-            onClick={() => setPageIndex((pageIndex) => pageIndex + 1)}
-          >
-            Next
-          </button>
-          <button
-            disabled={pageIndex === MAX_PAGES}
-            className='disabled:text-gray-400'
-            onClick={() => setPageIndex(MAX_PAGES)}
-          >
-            Go to last
-          </button>
+            <button
+              disabled={pageIndex === MAX_PAGES}
+              className='disabled:text-gray-400'
+              onClick={() => setPageIndex((pageIndex) => pageIndex + 1)}
+            >
+              Next
+            </button>
+            <button
+              disabled={pageIndex === MAX_PAGES}
+              className='disabled:text-gray-400'
+              onClick={() => setPageIndex(MAX_PAGES)}
+            >
+              Go to last
+            </button>
+          </div>
+          <Cart />
         </div>
-        <Cart />
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
